57dd05a553b6c0f78c32c01fb17c5cece055d736
giang2u <andre.giang8@etu.univ-lorraine.fr>
2018-10-10 11:11:50 +0200

    creation MurStandard + init labyrinth

---
+++ src/model/mur/Mur.java
@@ -0,0 +1,10 @@
+package model.mur;
+
+public class Mur {
+	protected int posX, posY;
+	
+	public Mur(int x, int y){
+		this.posX = x;
+		this.posY = y;
+	}
+}
---
+++ src/model/mur/MurNormal.java
@@ -0,0 +1,11 @@
+package model.mur;
+
+
+public class MurNormal extends Mur{
+	
+	
+	public MurNormal(int x, int y){
+		super(x,y);
+	}
+
+}
