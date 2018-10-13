af337c80baeae5b03806fe8b577797a5e18e9eb4
giang2u <giang.andre@outlook.com>
2018-10-13 15:23:15 +0200

    ajout carac vitesse

--- src/model/personnages/Personnage.java
+++ src/model/personnages/Personnage.java
@@ -3,13 +3,14 @@ package model.personnages;
 public abstract class Personnage {
 	protected int x, y;
 	protected int pointVie;
-	protected int defense, attaque;
+	protected int defense, attaque, vitesse;
 	public Personnage(int x, int y) {
 		this.pointVie = 1; 
 		this.attaque = 1;
 		this.defense = 0;
 		this.x = x;
 		this.y = y;
+		this.vitesse = 1;
 		
 	}
     public void goGauche(){
