fcb6144756536cd1073b3e6dae45b9c1f965f47e
cocosh03 <corentin.lexa@gmail.com>
2018-11-28 08:32:21 +0100

    Modif suite rm Ramassable

--- src/model/Item/Ramassable.java
+++
@@ -1,9 +0,0 @@
-package model.Item;
-//Permet de savoir si l'objet est ramassable
-public interface Ramassable {
-
-    boolean ramasser = false;
-
-    boolean estRamasser();
-    boolean setRamasser();
-}
